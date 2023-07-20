12b9002a02a8e7b87206af318bc8ac0093973804
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2023-07-20 16:35:58 -0300

    feat: request using fetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,57 @@ import './App.css'
 
 import ListRender from './components/ListRender'
 import ConditionalRender from './components/ConditionalRender'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Title from './components/Title'
 import Form from './components/Form'
-
-
+import { useFetch } from './hooks/useFetch'
+const url = 'http://localhost:3000/products'
 
 function App() {
 
-  const [colorAttribute, setColorAttribute] = useState(true)
+  const [products, setProducts] = useState([])
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
 
-  const changeColor = () => {
-    setColorAttribute(!colorAttribute)
+  const { data: items, httpConfig } = useFetch(url)
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const product = {
+      name,
+      price
+    }
+
+    httpConfig(product, 'POST')
+
+    setName('')
+    setPrice('')
   }
 
   return (
     <div className='App'>
       <h1>Fundamentos React</h1>
-      {/* <h2 className={colorAttribute ? 'red-title' : 'title' }>Este título vai ter classe dinâmica</h2>
-      <button onClick={changeColor}>Change Color!</button> */}
-      <Form user={{ name: 'Josias', email: 'josias@gmail.com' }}/>
+      <ul>
+        {items && items.map(product => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input type="text" value={name} name='name' onChange={e => setName(e.target.value)} />
+          </label>
+          <label>
+            Preço:
+            <input type="number" value={price} name='price' onChange={e => setPrice(e.target.value)} />
+          </label>
+          <input type="submit" value='Criar produto' />
+        </form>
+      </div>
+
     </div>
   )
 }
